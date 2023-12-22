0c2f9185a704ae42503f3e71c7ccd23ad6b71210
ranveer.verma@gmail.com <ranveer.verma@gmail.com>
2023-12-22 21:04:31 +0000

    DEVELOPING STAGE ORDER

---
+++ models/raw/stg_orders.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice,
+o.ordercostprice,
+o.ordersellingprice-o.ordercostprice as orderprofit,
+--customer data
+cus.customername,
+cus.segment,
+cus.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as cus on o.customerid=cus.CUSTOMERID
+Left join {{ ref('raw_product') }} as p on o.productid = p.productid
